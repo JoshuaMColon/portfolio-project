6c9ac1eb8d9d589a32fbd04fdd44b3fdf8781cb7
jcolon117-sketch <zylence16@gmail.com>
2026-02-25 21:02:40 -0500

    updated main.js under the SWIPER PROJECTS comment, and index.html

--- assets/js/main.js
+++ assets/js/main.js
@@ -5,7 +5,23 @@
 
 
 /*=============== SWIPER PROJECTS ===============*/
-
+let swiperProjects = new Swiper(".projects_container", {
+    loop: true,
+    spaceBetween: 24,
+    navigation: {
+        nextEl: ".swiper-button-next",
+        prevEl: ".swiper-button-prev",
+    },
+    pagination: {
+        el: ".swiper-pagination",
+    },
+    breakpoints: {
+        1200: {
+            slidesPerView: 2,
+            spaceBetween: -56,
+        },
+    },
+});
 
 /*=============== SWIPER TESTIMONIAL ===============*/
 
